0c5624f82111b51a12fcbb647ffde4c5594db7b3
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2025-01-23 12:43:56 +0100

    Implemented react routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,18 @@
-import { useState, useEffect } from "react";
-import CustomLoadingIndicator from "../src/components/CustomLoader/CustomLoadingIndicator";
-import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
-import Carousel from "./components/Carousel/Carousel";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
-import Footer from "./components/Footer/Footer";
-import Certification from "./components/Certifications/Certification";
-import Button from "./components/WhatsApp/Button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import Contact from "./Pages/Contact";
+import ScrollToTop from "./components/Scroll/ScrollToTop";
 
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 20000); // Adjust duration as needed
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
-    <div>
-      {isLoading ? (
-        <CustomLoadingIndicator />
-      ) : (
-        <div>
-          <Navbar />
-          <main>
-            <Hero />
-            <Carousel />
-            <About />
-            <Projects />
-            <Certification />
-          </main>
-          <footer>
-            <Footer />
-          </footer>
-          <Button />
-        </div>
-      )}
-    </div>
+    <Router>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 };
 
