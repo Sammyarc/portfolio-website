f2029ac782ce686582438aa276533c0ff5b06b39
Sammyarc <nkemakolamsam@gmail.com>
2025-04-17 23:11:19 +0100

    Added additional pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import ScrollToTop from "./components/Scroll/ScrollToTop";
+import Project from "./Pages/Project";
+import Profile from "./Pages/Profile";
 
 
 const App = () => {
@@ -10,7 +12,10 @@ const App = () => {
       <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Project />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} /> {/* Redirect to Home for any other route */}
+        <Route path="/about" element={<Profile />} />
       </Routes>
     </Router>
   );
