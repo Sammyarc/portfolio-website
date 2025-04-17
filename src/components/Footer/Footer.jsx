07b2bff9ad98d4bfb2369675bb41168f344e8b9d
Sammyarc <nkemakolamsam@gmail.com>
2025-04-17 23:13:21 +0100

    Modifications

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   
     return (
       <footer className=" text-white mt-[8vw] mb-[3vw] text-center md:mt-[3vw] md:mb-[2vw]">
-        <p className="text-[4.2vw] font-Poppins md:text-[1.2vw]">
+        <p className="text-[4.2vw] font-Poppins md:text-[1vw]">
           &copy; {currentYear} DevSammy, &nbsp; All Rights Reserved.
         </p>
       </footer>
